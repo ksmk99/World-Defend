3b161176247b048c116b24970f1fad28cc1b50c5
Maxim Kosarev <Kosarev.me@thegamix.com>
2023-10-01 16:23:52 +0300

    BulletView

--- Assets/Scripts/Unit/Weapon/Bullet/View/BulletView.cs
+++ Assets/Scripts/Unit/Weapon/Bullet/View/BulletView.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
+using Zenject;
+using static Zenject.SignalSubscription;
 
 namespace Unit.Bullet
 {
-    public class BulletView : MonoBehaviour, IBulletView
+    public class BulletView : MonoBehaviour, IBulletView, IPoolable<BulletRuntimeSettings, IMemoryPool>, IDisposable
     {
+        private IMemoryPool _pool;
+
+        public event Action<Collider> OnCollide;
+        public event Action<BulletView> OnDispose;
+        public event Action<BulletRuntimeSettings> OnReinitialize;
+
+        public void Dispose()
+        {
+            _pool.Despawn(this);
+        }
+
+        public void OnDespawned()
+        {
+            _pool = null;
+        }
+
+        public void OnSpawned(BulletRuntimeSettings p1, IMemoryPool p2)
+        {
+            _pool = p2;
+            OnReinitialize?.Invoke(p1);
+        }
+
         public void OnTriggerEnter(Collider other)
+        {
+            OnCollide?.Invoke(other);
+        }
+
+        public class Factory : PlaceholderFactory<BulletRuntimeSettings, BulletView>
         {
 
         }
